385e1d584f9dc4be965784233545318a5f58889a
dyundyun2910 <dyundyun2910@gmail.com>
2023-09-12 00:05:05 +0000

    Inputからリストへの追加処理を実装

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,18 @@ import "./styles.css";
 const onClickAdd = () => {
     const inputText = document.getElementById("add-text").value;
     document.getElementById("add-text").value = "";
+
+    const div = document.createElement("div");
+    div.className = "list-row";
+    console.log(div);
+
+    const li = document.createElement("li");
+    li.innerText = inputText;
+
+    div.appendChild(li);
+
+    document.getElementById("incomplete-list").appendChild(div);
+
 };
 
 document.getElementById("add-button").addEventListener("click", () => onClickAdd());
